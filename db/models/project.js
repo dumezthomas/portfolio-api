a9bb9a1f048bcc7a888d412404aec5bd5dc23d06
Thomas Dumez <dumezthomas@gmail.com>
2023-02-06 16:32:08 -0800

    update project db model

--- db/models/project.js
+++ db/models/project.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const projectSchema = new Schema({
   project: { type: String, required: true, maxlength: 64 },
   client: { type: String, required: true, maxlength: 32 },
-  website: { type: String, required: true, maxlength: 128 },
+  website: { type: String, maxlength: 128 },
+  github: { type: String, maxlength: 128 },
   technologies: { type: String, required: true, maxlength: 64 },
   description: { type: String, required: true },
   userId: { type: String, required: true },
